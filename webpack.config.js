a91ca709f727673729c6f429a982b0cff08cb3d6
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-12-13 18:35:17 -0700

    converted site into a pwa

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 const webpack = require("webpack");
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 // const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
@@ -46,21 +47,29 @@ const config = {
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: 'static'
+    }),
+    // new invokes a constructor function, after instantiating, we provide a single object as our only argument
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      // specifies the homepage for the PWA relative to the location of the manifest file
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      // fingerprint/inject are specific to the manifest plugin
+      //fingerprint tells webpack wether or not to gnerate unique fingerprints so that each time a new manifest is generated, it looks like this: manifest.lhge325d.json
+      fingerprints: false,
+      // determines whether the link to the manifest.json is added to the HTML
+      // since we arent' using fingerprints, we can set this to false.
+      inject: false,
+      icons: [{
+        src: path.resolve("assets/img/icons/icon-512x512.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        // designates where the icon will be sent after the creation of the web manifest is completed by the plugin
+        destination: path.join("assets", "icons")
+      }]
     })
-    // new WebpackPwaManifest({
-    //   name: "Food Event",
-    //   short_name: "Foodies",
-    //   description: "An app that allows you to view upcoming food events.",
-    //   background_color: "#01579b",
-    //   theme_color: "#ffffff",
-    //   fingerprints: false,
-    //   inject: false,
-    //   icons: [{
-    //     src: path.resolve("assets/img/icons/icon-512x512.png"),
-    //     sizes: [96, 128, 192, 256, 384, 512],
-    //     destination: path.join("assets", "icons")
-    //   }]
-    // })
   ],
   mode: 'development'
 };
